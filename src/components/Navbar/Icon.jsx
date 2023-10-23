2cc485995c294174095d4464f3b2c6ec8392eecf
Abdullah2412 <103569580+Abdullah2412@users.noreply.github.com>
2023-10-24 03:26:51 +0500

    Initial commit

---
+++ src/components/Navbar/Icon.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Icon({ children, setActive, index, isActive, theme }) {
+  const className = "";
+  // const className = theme === "dark" ? "bg-white text-black" : "bg-black text-white"
+
+  return (
+    <div
+      className={`
+    ${isActive === index ? className : ""} 
+    relative rounded-full overflow-hidden group`}
+    >
+      <div
+        className={`text-base px-4 py-1 cursor-pointer transition-all duration-400 group-hover:scale-75`}
+        onClick={() => {
+          setActive(index);
+        }}
+      >
+        {children}
+      </div>
+      <div
+        className={`bg-[#fa1b1b] text-white text-base px-4 py-1 rounded-full cursor-pointer transition-all duration-400 group-hover:top-0 top-[105%] left-1/2 -translate-x-1/2 absolute `}
+        onClick={() => {
+          setActive(index);
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
